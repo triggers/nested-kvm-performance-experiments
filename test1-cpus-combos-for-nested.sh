849c4b774dbd1e8ee175de42d59d368b4b67b07b
Richard Potter <potter@axsh.net>
2014-11-28 23:05:20 +0900

    Added working test1-cpus-combos-for-nested.sh

---
+++ test1-cpus-combos-for-nested.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cat "$0" >>nested-kvm-tests.log
+
+echo =============================================================================
+
+./nested-kvm-ctrl.sh 4 -kill
+./nested-kvm-ctrl.sh 3 -kill
+
+for hostcpus in 1 2 4 8; do
+
+    ./nested-kvm-ctrl.sh 3 -kill
+    ./nested-kvm-ctrl.sh 3 -boot k3cpus=$hostcpus
+
+    for guestcpus in 1 2 4; do
+
+	echo "====================== hosts: $hostcpus =========== guests: $guestcpus ================================="
+
+	./nested-kvm-ctrl.sh 4 -kill
+	./nested-kvm-ctrl.sh 4 -boot vmcpus=$guestcpus
+
+    done
+
+done
+
+echo Finished              test                   1
+
+echo =============================================================================
+
+# first results:
+# host   nested
+# cpus  cpus      time
+# 1	1	115.676
+# 2	1	74.914  (surprise)
+# 4	1	59.94
+# 8	1	59.026
+# 1	2	60.005
+# 2	2	51.909
+# 4	2	54.032
+# 8	2	54.078
+# 1	4	72.634
+# 2	4	56.471  (worse than 2 2, because guest > host??)
+# 4	4	54.106
+# 8	4	54.086
+# 
+# Conclusions: 
+#  - one cpu on host or nested is bad.
+#  - after 2 2, not much difference
